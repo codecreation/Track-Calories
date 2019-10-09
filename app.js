5b18754eb9d77d327ef84973f70d18b88bad9a88
Amir Tamimi <amir.tamim@trojans.dsu.edu>
2019-10-08 19:45:32 -0700

    Add items to list

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const ItemCtrl = (function(){
 
   const data = {
     items: [
-      {id: 0, name: 'Steak Dinner', calories: 450},
-      {id: 1, name: 'Eggs', calories: 175},
-      {id: 2, name: 'Cookies', calories: 240}
+      // {id: 0, name: 'Steak Dinner', calories: 450},
+      // {id: 1, name: 'Eggs', calories: 175},
+      // {id: 2, name: 'Cookies', calories: 240}
     ],
     currentItem: null,
     totalCalories: 0
@@ -28,6 +28,25 @@ const ItemCtrl = (function(){
     },
     addItem: function(name, calories) {
       console.log(name, calories);
+      let id;
+      //create ID
+      if(data.items.length > 0)
+      {
+        id = data.items[data.items.length - 1].id + 1;
+      }else{
+        id = 0;
+      }
+
+      //calories to number
+      calories = parseInt(calories);
+
+      //create new item
+      newItem = new Item(id, name, calories);
+
+      data.items.push(newItem);
+
+      return newItem;
+    //  data.items.totalCalories += calories;
     }
   }
 })();
@@ -45,6 +64,7 @@ const UICtrl = (function(){
   return{
     populateItemList: function(items) {
       let html = '';
+      console.log("At populate");
 
       items.forEach(function(item) {
         html+= `<li id="item-${item.id}" class="collection-item">
@@ -62,6 +82,21 @@ const UICtrl = (function(){
         name: document.querySelector(UISelectors.itemNameInput).value,
         calories: document.querySelector(UISelectors.itemCaloriesInput).value,
       }
+    },
+    addListItem: function(item) {
+      //create li element
+      const li = document.createElement('li');
+      //add class
+      li.className = 'collection-item';
+      //add id
+      li.id = `item-${item.id}`;
+      //add HTML
+      li.innerHTML = `<strong>${item.name}: </strong><em>${item.calories} Calories</em>
+      <a href="" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>`
+      //insert item
+      document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
     },
     getSelectors: function() {
       return UISelectors;
@@ -88,10 +123,12 @@ const App = (function(ItemCtrl, UICtrl){
     {
        console.log("Got here");
        const newItem =ItemCtrl.addItem(input.name, input.calories);
+       UICtrl.addListItem(newItem);
 
     }
 
     e.preventDefault();
+
   }
 
   return{
